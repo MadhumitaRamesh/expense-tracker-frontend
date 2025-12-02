567ec220843d1379fb445ccce3dae8720461eaed
Madhumita Ramesh <madhumita@Madhumitas-MacBook-Air.local>
2025-12-02 18:29:59 +0530

    Added API with JWT token

---
+++ src/services/api.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: 'http://localhost:8080/api', // Backend address
+});
+
+// Add JWT token to every request (boss's authorisation mechanism)
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`; // Send token
+  }
+  return config;
+});
+
+export default api;
