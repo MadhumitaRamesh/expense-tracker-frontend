2c6bdf69d06337db12f8a1848bee9c80dc76f9fa
MadhumitaRamesh <p6cm4ywhmt@privaterelay.appleid.com>
2025-12-05 01:49:20 +0530

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -9,7 +9,7 @@ function Login() {
 
   const handleLogin = async () => {
     try {
-      const res = await axios.post('http://localhost:8080/api/auth/login', {
+      const res = await axios.post(`${process.env.REACT_APP_API_URL}/api/auth/login`, {
         username,
         password,
       });
