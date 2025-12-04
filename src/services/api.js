ab3c07270abe8f7ebd09a5606f4c289c887f1a83
Madhumita Ramesh <madhumita@Madhumitas-MacBook-Air.local>
2025-12-04 23:47:15 +0530

    Configure for Render deployment: Use Env Var for API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api', // Backend address
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080/api', // Backend address
 });
 
 // Add JWT token to every request (boss's authorisation mechanism)
